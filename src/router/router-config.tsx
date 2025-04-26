2aa79c75a32c72e75e3e61a5aa740c808377887a
bipinstha07 <bipinshretha830@gmail.com>
2025-04-25 23:17:12 -0500

    second deleted contact

--- src/router/router-config.tsx
+++ src/router/router-config.tsx
@@ -1,16 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "../App";
-import Contact from "../contact/contact";
+
 
 const routerObj  = createBrowserRouter(
     [
         {path:'/',
             element: <App/>
-        },
-        {
-            path:'/contact',
-            element: <Contact/>
         }
+    
     ]
 )
 
