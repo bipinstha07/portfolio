74877dff8a36047b3f5413a5b5f9f4120bc10c8f
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-05-13 22:07:12 -0500

    correct

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,6 +1,4 @@
-import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FaInstagramSquare } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import logo from '../assets/logo.png'
 
