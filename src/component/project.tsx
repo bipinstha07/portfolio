ae2a2670fad56a72f820dd303354bf47833ad53c
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-08-10 02:24:50 -0500

    fix height and width

--- src/component/project.tsx
+++ src/component/project.tsx
@@ -35,7 +35,7 @@ function ImageGallery({ images, title }: { images: string[], title: string }) {
     return (
         <div className="relative group">
             <img
-                className="w-[380px] h-48 lg:h-60 mx-auto object-cover transition-transform duration-500 group-hover:scale-100 rounded"
+                className="w-[580px] h-52 lg:h-80 mx-auto object-cover transition-transform duration-500 group-hover:scale-100 rounded"
                 src={images[currentImageIndex]}
                 alt={title + ' image ' + (currentImageIndex + 1)}
             />
@@ -160,7 +160,7 @@ function Project() {
                             <div className="flex flex-col lg:flex-row">
                                 {/* Image Container - Left Side */}
                                 <motion.div 
-                                    className="lg:w-2/5 mt-10  relative sm:mb-10 h-52 lg:h-auto overflow-hidden"
+                                    className="lg:w-3/5 mt-2  relative sm:mb-10 h-52 lg:h-auto overflow-hidden"
                                     whileInView={{ x: 0, opacity: 1 }} 
                                     initial={{ x: -50, opacity: 0 }} 
                                     transition={{ duration: 0.5, delay: 0.1 }}
@@ -169,7 +169,7 @@ function Project() {
                                         <ImageGallery images={data.images} title={data.title} />
                                     ) : (
                                         <motion.img 
-                                            className=" h-48 lg:h-60 mx-auto object-contain transition-transform duration-500 group-hover:scale-100" 
+                                            className=" h-58 lg:h-60 mx-auto object-contain transition-transform duration-500 group-hover:scale-100" 
                                             src={data.images[0]} 
                                             alt={data.title}
                                         />
